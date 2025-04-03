e32267583e983ca1bc2d901ef7044fa5f516b6af
wiso-william <williammancy0@gmail.com>
2025-04-03 19:58:49 +0200

    Backend concluso problema con db e lombok risolto

--- src/main/java/it/medea/pdfextraction/model/Profile.java
+++ src/main/java/it/medea/pdfextraction/model/Profile.java
@@ -1,6 +1,7 @@
 package it.medea.pdfextraction.model;
 
-import java.time.LocalDate;
+
+import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +11,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "profiles")
+@Document(collection = "md_dati_estratti")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,7 +25,7 @@ public class Profile {
     
     private String codiceFiscale;
     
-    private LocalDate birthDate;  // Data di nascita (se disponibile)
+    private Date birthDate;  // Data di nascita (se disponibile)
     
     private Integer age;  // Età (se disponibile)
     
